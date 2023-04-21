fee1a13c99d02efaa6dc4bcaf4eb0c8b0978ae98
AnkitO2 <120965263+AnkitO2@users.noreply.github.com>
2023-04-21 23:31:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Productcart from "./components/Productcart";
+
+function App() {
+  return (
+    <div className="App">
+    <Productcart/>
+    </div>
+  );
+}
+
+export default App;
